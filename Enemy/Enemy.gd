4a1b232d5b3f44694166933a37e1dd0183ef7e7b
mdisko <112019630+mdisko@users.noreply.github.com>
2022-11-15 18:01:13 -0500

    Add files via upload

---
+++ Enemy/Enemy.gd
@@ -0,0 +1,10 @@
+extends Area
+
+
+
+func _on_Enemy_body_entered(body):
+	if body.name == "Player":
+		var sound = get_node_or_null("/root/Game/Zombie")
+		if sound != null:
+			sound.playing = true
+		queue_free()
